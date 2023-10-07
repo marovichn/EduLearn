635a72918edad44076f6dc7a56eed9a8bf085b0a
marovichn <nikolamarovic.11@gmail.com>
2023-10-07 19:09:06 +0200

    Logo, Metadata fix, Clerk setup

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import Link from "next/link";
 import { MdCastForEducation } from "react-icons/md";
 
-
 interface LogoProps {}
 
 const Logo: FC<LogoProps> = ({}) => {
@@ -10,7 +9,7 @@ const Logo: FC<LogoProps> = ({}) => {
     <Link href='/dashboard' className='flex items-center'>
       <MdCastForEducation className='w-12 h-12 text-yellow-300 mr-4' />
       <span className='self-center text-3xl font-semibold whitespace-nowrap text-white'>
-        EduGrade
+        EduLearn
       </span>
     </Link>
   );
