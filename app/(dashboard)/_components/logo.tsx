5d89ff080b2ba68088b65fa042b3bbe5690fea09
marovichn <nikolamarovic.11@gmail.com>
2023-10-09 01:06:19 +0200

    UI fixes

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Logo = () => {
   return (
     <Link href='/' className='flex items-center p-0'>
-      <MdCastForEducation className='text-yellow-300 mr-2' size={40}/>
+      <MdCastForEducation className='text-yellow-300 mr-4' size={40}/>
       
       <span className='self-center text-3xl font-bold whitespace-nowrap text-white'>
         EduLearn
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar = () => {
   return (
-    <div className='p-4 border-b h-full flex items-center  shadow-sm bg-gray-700'>
+    <div className='p-4 border-b h-full flex items-center  shadow-sm bg-gray-700 rounded-br-xl max-md:rounded-b-xl'>
       <MobileSidebar />
       <NavbarRoutes />
     </div>
